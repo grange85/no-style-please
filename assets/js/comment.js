e35850ba5f72ba588169195a1e309f941f4dbbe0
Andy Aldridge <andy@grange85.co.uk>
2024-08-02 22:54:12 +0100

    update

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -1,12 +1,13 @@
 var this_script = document.getElementById("comment-script");
 var sectionid = this_script.getAttribute('data-sectionid');
+var sitename = this_script.getAttribute('data-sitename');
 
 document.getElementById("comment-section").innerHTML = sectionid;
 
 initComments({
     node: document.getElementById("comment-section"),
-    defaultHomeserverUrl: "https://matrix.cactus.chat:8448",
-    serverName: "cactus.chat",
-    siteName: "ahfow",
-    commentSectionId: sectionid
+    defaultHomeserverUrl: "https://matrix.org
+    serverName: "matrix.org",
+    siteName: sitename,
+    commentSectionId: sitename + "-" + sectionid
   })
