3f05f12f94c44d114b35b6cd93a2b32e8888576d
Andy Aldridge <andy@grange85.co.uk>
2024-08-03 17:02:48 +0100

    update js

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -2,7 +2,7 @@ var this_script = document.getElementById("comment-script");
 var sectionid = this_script.getAttribute('data-sectionid');
 var sitename = this_script.getAttribute('data-sitename');
 
-document.getElementById("comment-section").innerHTML = sectionid;
+//document.getElementById("comment-section").innerHTML = sectionid;
 
 initComments({
     node: document.getElementById("comment-section"),
